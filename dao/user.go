7cd7d6fe74e814213fe14cca021744e237c73279
jake <jake@aresflare.com>
2019-11-02 20:46:06 +0700

    增加sql builder示例

--- dao/user.go
+++ dao/user.go
@@ -1,10 +1,14 @@
 package dao
 
-import "database/sql"
+import (
+	"database/sql"
+	qb "github.com/didi/gendry/builder"
+	"nomal-gin/utils/errlog"
+)
 
 type User struct {
 	Name string
-	Age int
+	Age  int
 }
 
 type UserDao struct {
@@ -12,15 +16,23 @@ type UserDao struct {
 }
 
 func NewUserDao(db *sql.DB) *UserDao {
-	return &UserDao{BaseDao:NewBaseDao(db)}
+	return &UserDao{BaseDao: NewBaseDao(db)}
 }
 
-// if not implement their method , while panic
+// if not implement their method , will panic
 
 func (ud *UserDao) TableName() string {
 	return "user"
 }
 
-func (ud *UserDao) GetAll(){
-
+func (ud *UserDao) GetAll(where map[string]interface{}, page, pageSize int) (interface{}, error) {
+	cond, vals, err := qb.BuildSelect(ud.TableName(), where, []string{"name", "count(price) as total", "age"})
+	if err != nil {
+		errlog.Error("sql build failed ", err)
+	}
+	rows, err := ud.db.Query(cond, vals...)
+	if err != nil {
+		errlog.Error("db execute failed ", err)
+	}
+	// todo marshal the rows which from db to users and return
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module nomal-gin
 go 1.12
 
 require (
+	github.com/didi/gendry v1.3.1
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-redis/redis v6.15.6+incompatible
-	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/jinzhu/gorm v1.9.11
 	github.com/shopspring/decimal v0.0.0-20191009025716-f1972eb1d1f5
 	github.com/swaggo/gin-swagger v1.2.0
 	go.uber.org/zap v1.12.0
 	google.golang.org/appengine v1.6.0 // indirect
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.2.2
 )
